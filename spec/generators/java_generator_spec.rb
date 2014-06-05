1388ce360a0e369f4c92f7176a7fed68663015e3
Steven Jones <steven@layer.com>
2014-06-05 16:59:10 -0700

    generate jars

--- spec/generators/java_generator_spec.rb
+++ spec/generators/java_generator_spec.rb
@@ -6,29 +6,30 @@ describe MonkeyButler::Generators::JavaGenerator do
   let!(:project_root) { clone_temp_sandbox }
 
   describe '#generate' do
-    it "should have a `generate` method" do
-      # Mostly here for project_root
-      puts "Working in directory: #{project_root}"
+    before(:each) do
       invoke!(['generate'])
     end
 
     it "should have a project/build.gradle file" do
-      invoke!(['generate'])
       expect(File.directory?(File.join(project_root, 'project'))).to eq(true)
       expect(File.file?(File.join(project_root, 'project/build.gradle'))).to eq(true)
     end
 
     it "should have a schema file in assets/schema" do
-      invoke!(['generate'])
       expect(File.directory?(File.join(project_root, 'project/monkeybutler/src/main/assets/schema'))).to eq(true)
       expect(File.file?(File.join(project_root, 'project/monkeybutler/src/main/assets/schema/schema_create.sql'))).to eq(true)
     end
 
     it "should have migration files in assets/migrations" do
-      invoke!(['generate'])
       expect(File.directory?(File.join(project_root, 'project/monkeybutler/src/main/assets/migrations'))).to eq(true)
       expect(Dir.entries(File.join(project_root, 'project/monkeybutler/src/main/assets/migrations')).size).not_to eq(2)
     end
+
+    it "should have project/monkeybutler/build/libs/monkeybutler.jar, monkeybutler-javadoc.jar files" do
+      expect(File.file?(File.join(project_root, 'project/monkeybutler/build/libs/monkeybutler.jar'))).to eq(true)
+      expect(File.file?(File.join(project_root, 'project/monkeybutler/build/libs/monkeybutler-javadoc.jar'))).to eq(true)
+    end
+
   end
 
 end
