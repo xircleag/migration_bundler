c093815e388bbc03fe2ce928f4008e390f4f9923
Steven Jones <steven@layer.com>
2014-06-07 11:20:06 -0700

    simplified hasSchema; added javadocs

--- lib/monkey_butler/generators/java/project/src/main/java/com/layer/sdk/monkeybutler/datasource/ResourceDataSource.java
+++ lib/monkey_butler/generators/java/project/src/main/java/com/layer/sdk/monkeybutler/datasource/ResourceDataSource.java
@@ -14,7 +14,6 @@ import com.layer.sdk.monkeybutler.schema.Schema;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.JarURLConnection;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -34,32 +33,46 @@ public class ResourceDataSource implements DataSource {
     private static final String MIGRATIONS_PATH = BASE + "/" + MIGRATIONS;
     private static final String SCHEMA_PATH = BASE + "/" + SCHEMA + "/mb_schema.sql";
 
+    /**
+     * Returns whether this DataSource has a Schema entry.
+     *
+     * @return A boolean representing the presence of a Schema entry in this DataSource.
+     */
     @Override
     public boolean hasSchema() {
-        Schema schema = getSchema();
-        InputStream in = null;
-        try {
-            in = schema.getStream();
-            if (in != null) {
-                return true;
-            }
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return false;
+        return (getSchema() != null);
     }
 
+    /**
+     * Returns the Schema entry if it exists, or `null` if it does not.
+     *
+     * @return The Schema entry if it exists, or `null` if it does not.
+     */
     @Override
     public Schema getSchema() {
-        return new ResourceSchema(SCHEMA_PATH);
+        try {
+            return new ResourceSchema(SCHEMA_PATH);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
     }
 
+    /**
+     * Returns a list of Migrations bundled in the java resources.
+     *
+     * We get resource migrations by first getting a known entry in the resource bundle (the
+     * schema) and then backing out to find migrations that may also be bundled in the resources.
+     * The JAR may either be accessed directly or after expansion on the filesystem, and each case
+     * needs its own migration search technique.
+     *
+     * JAR: iterate through all JAR entries, capturing those whose name starts with
+     * `resources/migrations`
+     *
+     * Filesystem: from the schema entry, back out two levels (to resources), then jump to the
+     * migrations directory and iterate over the files in that directory.
+     *
+     * @return a list of Migrations bundled in the java resources
+     */
     @Override
     public List<Migration> getMigrations() {
         List<Migration> migrations = new LinkedList<Migration>();
@@ -76,9 +89,10 @@ public class ResourceDataSource implements DataSource {
                     JarFile jar = urlcon.getJarFile();
                     Enumeration<JarEntry> entries = jar.entries();
                     while (entries.hasMoreElements()) {
+                        // Path is the item name in the JAR
                         String path = entries.nextElement().getName();
                         if (path.startsWith(MIGRATIONS_PATH)) {
-                            migrations.add(new ResourceMigration(ResourceMigration.Type.JAR, path));
+                            migrations.add(new ResourceMigration(path));
                         }
                     }
                 } else if (connection instanceof FileURLConnection) {
@@ -86,8 +100,9 @@ public class ResourceDataSource implements DataSource {
                     File resourceDir = new File(url.toURI()).getParentFile().getParentFile();
                     File migrationsDir = new File(resourceDir, MIGRATIONS);
                     for (File file : migrationsDir.listFiles()) {
-                        migrations.add(new ResourceMigration(ResourceMigration.Type.FILESYSTEM,
-                                file.getAbsolutePath()));
+                        // Resource path is still relative to the JAR (not the filesystem)
+                        String path = MIGRATIONS_PATH + "/" + file.getName();
+                        migrations.add(new ResourceMigration(path));
                     }
                 }
             }
