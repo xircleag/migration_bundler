b212d4cc3e887c2681f6b728f2e4c527bccc310f
Steven Jones <steven@layer.com>
2014-06-07 11:20:24 -0700

    tests for valid/invalid Schema and Migration paths

--- lib/monkey_butler/generators/java/project/src/test/java/com/layer/sdk/monkeybutler/MonkeyButlerTests.java
+++ lib/monkey_butler/generators/java/project/src/test/java/com/layer/sdk/monkeybutler/MonkeyButlerTests.java
@@ -1,6 +1,8 @@
 package com.layer.sdk.monkeybutler;
 
 import com.layer.sdk.monkeybutler.migrations.Migration;
+import com.layer.sdk.monkeybutler.migrations.ResourceMigration;
+import com.layer.sdk.monkeybutler.schema.ResourceSchema;
 import com.layer.sdk.monkeybutler.schema.Schema;
 
 import junit.framework.TestCase;
@@ -10,6 +12,38 @@ import java.util.List;
 import static org.fest.assertions.Assertions.assertThat;
 
 public class MonkeyButlerTests extends TestCase {
+    public void testGetInvalidSchema() throws Exception {
+        try {
+            new ResourceSchema("invalid/path");
+            fail("IllegalArgumentException was not thrown");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+    public void testGetValidSchema() throws Exception {
+        try {
+            new ResourceSchema("resources/schema/mb_schema.sql");
+        } catch (IllegalArgumentException e) {
+            fail("IllegalArgumentException was thrown");
+        }
+    }
+
+    public void testGetInvalidMigration() throws Exception {
+        try {
+            new ResourceMigration("invalid/path");
+            fail("IllegalArgumentException was not thrown");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+    public void testGetValidMigration() throws Exception {
+        try {
+            new ResourceSchema("resources/migrations/1402070000_Origin.sql");
+        } catch (IllegalArgumentException e) {
+            fail("IllegalArgumentException was thrown");
+        }
+    }
+
     public void testGetMigrations() throws Exception {
         List<Migration> migrations = MonkeyButler.getMigrations();
         assertThat(migrations).hasSize(1);
