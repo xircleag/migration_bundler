da7e63e74105da81d542e290dc3a7de58a228bcb
Steven Jones <steven@layer.com>
2014-06-07 15:11:19 -0700

    broke various generator steps into sub methods

--- lib/monkey_butler/generators/java/java_generator.rb
+++ lib/monkey_butler/generators/java/java_generator.rb
@@ -3,11 +3,13 @@ require 'monkey_butler/generators/base'
 module MonkeyButler
   module Generators
     class JavaGenerator < MonkeyButler::Generators::Base
-      def generate
+      def copy_project
         # Copy Android project
         FileUtils.mkdir_p "project"
         FileUtils.cp_r File.join(File.dirname(__FILE__), "project"), "."
+      end
 
+      def copy_sql
         # Clear, Create and Populate schema and migrations directories
         FileUtils.rm_rf "project/src/main/resources/resources/schema"
         FileUtils.rm_rf "project/src/main/resources/resources/migrations"
@@ -15,11 +17,19 @@ module MonkeyButler
         FileUtils.mkdir_p "project/src/main/resources/resources/migrations/"
         FileUtils.cp_r project.schema_path, "project/src/main/resources/resources/schema/mb_schema.sql"
         FileUtils.cp_r project.migrations_path, "project/src/main/resources/resources/"
+      end
 
+      def build_project
         # Build
         Dir.chdir("project") do
           `gradle clean build jar`
         end        
+      end
+
+      def generate
+        copy_project()
+        copy_sql()
+        build_project()
       end
 
       def push
