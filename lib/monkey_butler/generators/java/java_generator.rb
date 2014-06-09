057c1753d2a737cc2bb5c90d088529d9228885f7
Steven Jones <steven@layer.com>
2014-06-09 15:05:16 -0700

    tests pass with new generator; still had to use `FileUtils.cp_r project.schema_path` and `FileUtils.cp_r project.migrations_path` for project.* to work

--- lib/monkey_butler/generators/java/java_generator.rb
+++ lib/monkey_butler/generators/java/java_generator.rb
@@ -3,41 +3,66 @@ require 'monkey_butler/generators/base'
 module MonkeyButler
   module Generators
     class JavaGenerator < MonkeyButler::Generators::Base
-      def copy_project
-        # Copy Android project
-        FileUtils.mkdir_p "project"
-        FileUtils.cp_r File.join(File.dirname(__FILE__), "project"), "."
-      end
-
-      def copy_sql
-        # Clear, Create and Populate schema and migrations directories
-        FileUtils.rm_rf "project/src/main/resources/resources/schema"
-        FileUtils.rm_rf "project/src/main/resources/resources/migrations"
-        FileUtils.mkdir_p "project/src/main/resources/resources/schema/"
-        FileUtils.mkdir_p "project/src/main/resources/resources/migrations/"
-        FileUtils.cp_r project.schema_path, "project/src/main/resources/resources/schema/mb_schema.sql"
-        FileUtils.cp_r project.migrations_path, "project/src/main/resources/resources/"
-      end
-
-      def build_project
-        # Build
-        Dir.chdir("project") do
-          `gradle clean build jar`
-        end        
+      def init
+        unless project.config['java.maven.url']
+          project.config['java.maven.url'] = ask("What is the URL of your Java Maven repo? ")
+        end
+        unless project.config['java.maven.username']
+          project.config['java.maven.username'] = ask("What is the username for your Java Maven repo? ")
+        end
+        unless project.config['java.maven.password']
+          project.config['java.maven.password'] = ask("What is the password for your Java Maven repo? ")
+        end
       end
 
       def generate
-        copy_project()
-        copy_sql()
-        build_project()
+        invoke :validate
+        empty_directory "project"
+        empty_directory "project/src"
+        empty_directory "project/src/main"
+        empty_directory "project/src/main/resources"
+        empty_directory "project/src/main/resources/schema"
+
+        copy_file "project/build.gradle", "project/build.gradle"
+        FileUtils.cp_r project.schema_path, "project/src/main/resources/schema/schema.sql"
+        FileUtils.cp_r project.migrations_path, "project/src/main/resources"
+        
+        run "cd project && gradle -Pversion=#{migrations.latest_version} clean jar"
+      end
+
+      def validate
+        fail Error, "Invalid configuration: java.repo is not configured." if java_url.nil?
+        fail Error, "Invalid configuration: java.username is not configured." if java_username.nil?
+        fail Error, "Invalid configuration: java.password is not configured." if java_password.nil?
       end
 
       def push
-        # Build
+        invoke :validate
         Dir.chdir("project") do
-          `gradle -Pusername=admin -Ppassword=admin123 clean bumpVersion build jar publish`
-        end        
+          run "gradle -Pversion=#{migrations.latest_version} -Purl=#{java_url} -Pusername=#{java_username} -Ppassword=#{java_password} publish"
+        end
       end
+
+      private
+
+      def java_url
+        project.config['java.maven.url']
+      end
+
+      def java_username
+        project.config['java.maven.username']
+      end
+
+      def java_password
+        project.config['java.maven.password']
+      end
+
+#      def push
+#        # Build
+#        Dir.chdir("project") do
+#          `gradle -Pusername=admin -Ppassword=admin123 clean publish`
+#        end        
+#      end
     end
   end
 end
--- spec/generators/java_generator_spec.rb
+++ spec/generators/java_generator_spec.rb
@@ -1,49 +1,47 @@
 require 'spec_helper'
+require 'monkey_butler/generators/base'
 require 'monkey_butler/generators/java/java_generator'
 
 describe MonkeyButler::Generators::JavaGenerator do
   let(:thor_class) { MonkeyButler::Generators::JavaGenerator }
   let!(:project_root) { clone_temp_sandbox }
 
-  def get_version
-    Dir.chdir(File.join(project_root, 'project')) do
-      return `cat version.gradle | grep -oe "['][^']*[']"`.strip.delete "'"
-    end    
-  end
-
-  def get_jar_path
-    return File.join(project_root, 'project/build/libs/monkeybutler-' + get_version + '.jar')
+  describe "#init" do
+    it "asks for Maven Java repository URL and credentials" do
+      puts "Working in directory: #{project_root}\n"
+      set_config
+    end
   end
 
   describe '#generate' do
     before(:each) do
       puts "Working in directory: #{project_root}\n"
+      set_config
       invoke!(['generate'])
     end
 
-    it "should have project/build.gradle and version.gradle files" do
+    it "should have project/build.gradle" do
       expect(File.directory?(File.join(project_root, 'project'))).to eq(true)
       expect(File.file?(File.join(project_root, 'project/build.gradle'))).to eq(true)
-      expect(File.file?(File.join(project_root, 'project/version.gradle'))).to eq(true)
     end
 
-    it "should have a schema file in resources/resources/schema" do
-      expect(File.directory?(File.join(project_root, 'project/src/main/resources/resources/schema'))).to eq(true)
-      expect(File.file?(File.join(project_root, 'project/src/main/resources/resources/schema/mb_schema.sql'))).to eq(true)
+    it "should have a schema file in resources/schema" do
+      expect(File.directory?(File.join(project_root, 'project/src/main/resources/schema'))).to eq(true)
+      expect(File.file?(File.join(project_root, 'project/src/main/resources/schema/schema.sql'))).to eq(true)
     end
 
-    it "should have migration files in resources/resources/migrations" do
-      expect(File.directory?(File.join(project_root, 'project/src/main/resources/resources/migrations'))).to eq(true)
-      expect(Dir.entries(File.join(project_root, 'project/src/main/resources/resources/migrations')).size).not_to eq(2)
+    it "should have migration files in resources/migrations" do
+      expect(File.directory?(File.join(project_root, 'project/src/main/resources/migrations'))).to eq(true)
+      expect(Dir.entries(File.join(project_root, 'project/src/main/resources/migrations')).size).not_to eq(2)
     end
 
     it "should have project/build/libs/monkeybutler-[version].jar files" do
-      expect(File.file?(get_jar_path)).to eq(true)
+      expect(File.file?(jar_path)).to eq(true)
     end
 
     it "should have a schema packaged in the JAR" do
-      jar_schema = `jar tf #{get_jar_path} | grep -e resources/schema/.*[.]sql`.strip
-      expect(jar_schema).to eq("resources/schema/mb_schema.sql")
+      jar_schema = `jar tf #{jar_path} | grep -e schema/.*[.]sql`.strip
+      expect(jar_schema).to eq("schema/schema.sql")
     end
 
     it "should have all migrations packaged in the JAR" do
@@ -57,7 +55,7 @@ describe MonkeyButler::Generators::JavaGenerator do
       end
       
       # Create a array of all migrations packaged in the JAR
-      jar_migrations = `jar tf #{get_jar_path} | grep -e resources/migrations/.*[.]sql`
+      jar_migrations = `jar tf #{jar_path} | grep -e migrations/.*[.]sql`
       jar_migration_array = jar_migrations.split(/\r?\n/)
 
       # Verify that the expected and actual have the same size
@@ -65,16 +63,46 @@ describe MonkeyButler::Generators::JavaGenerator do
 
       # Verify that all JAR'd migrations were expected
       jar_migration_array.each { |jar_migration|
-        length = jar_migration.length - "resources/migrations/".length
+        length = jar_migration.length - "migrations/".length
         migration = jar_migration[-length, length]
         expect(expected_migrations[migration]).to eq(true)
       }
     end
-
-    it "should have a MonkeyButler.class packaged in the JAR" do
-      jar_class = `jar tf #{get_jar_path} | grep -e "com/layer/sdk/monkeybutler/MonkeyButler.class"`.strip
-      expect(jar_class).to eq("com/layer/sdk/monkeybutler/MonkeyButler.class")
-    end
-
   end
+
+  def remove_java_repo_from_config
+    yaml_path = File.join(project_root, '.monkey_butler.yml')
+    project = YAML.load(File.read(yaml_path))
+    project['config'].delete 'java.maven.url'
+    File.open(yaml_path, 'w') { |f| f << YAML.dump(project) }
+  end
+
+  def remove_java_username_from_config
+    yaml_path = File.join(project_root, '.monkey_butler.yml')
+    project = YAML.load(File.read(yaml_path))
+    project['config'].delete 'java.maven.username'
+    File.open(yaml_path, 'w') { |f| f << YAML.dump(project) }
+  end
+
+  def remove_java_password_from_config
+    yaml_path = File.join(project_root, '.monkey_butler.yml')
+    project = YAML.load(File.read(yaml_path))
+    project['config'].delete 'java.maven.password'
+    File.open(yaml_path, 'w') { |f| f << YAML.dump(project) }
+  end
+
+  def set_config
+    remove_java_repo_from_config
+    remove_java_username_from_config
+    remove_java_password_from_config
+    expect(Thor::LineEditor).to receive(:readline).with("What is the URL of your Java Maven repo?  ", {}).and_return("maven")
+    expect(Thor::LineEditor).to receive(:readline).with("What is the username for your Java Maven repo?  ", {}).and_return("none")
+    expect(Thor::LineEditor).to receive(:readline).with("What is the password for your Java Maven repo?  ", {}).and_return("none")
+    invoke!(['init'])
+  end
+
+  def jar_path
+    return File.join(project_root, "project/build/libs/monkeybutler-201405233443021.jar")
+  end
+
 end
