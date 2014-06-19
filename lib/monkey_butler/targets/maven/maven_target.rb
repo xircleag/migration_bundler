1050c1e410c7c5403400463a2b1381e6fc3b78ff
Blake Watters <blake@layer.com>
2014-06-18 17:16:04 -0700

    Fix secondary bug in `maven push`

--- lib/monkey_butler/targets/maven/maven_target.rb
+++ lib/monkey_butler/targets/maven/maven_target.rb
@@ -28,7 +28,6 @@ module MonkeyButler
         FileUtils.cp_r project.schema_path, "project/src/main/resources/schema/schema.sql"
         FileUtils.cp_r project.migrations_path, "project/src/main/resources"
 
-        version = unique_tag_for_version(migrations.latest_version)
         run "cd project && gradle#{options['quiet'] && ' -q '} -Pversion=#{version} clean jar"
       end
 
@@ -40,7 +39,7 @@ module MonkeyButler
 
       def push
         invoke :validate
-        run "cd project && gradle#{options['quiet'] && ' -q'} -Pversion=#{migrations.latest_version} -Purl=#{maven_url} -Pusername=#{maven_username} -Ppassword=#{maven_password} publish"
+        run "cd project && gradle#{options['quiet'] && ' -q'} -Pversion=#{version} -Purl=#{maven_url} -Pusername=#{maven_username} -Ppassword=#{maven_password} publish"
       end
 
       private
@@ -55,6 +54,10 @@ module MonkeyButler
 
       def maven_password
         project.config['maven.password']
+      end
+
+      def version
+        unique_tag_for_version(migrations.latest_version)
       end
     end
   end
